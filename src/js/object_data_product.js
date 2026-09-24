c4a99e0e06c92f03e710ca2a849fdef7f207d13e
gha211th <agh.arrafif@gmail.com>
2026-09-24 13:11:57 +0700

    fix the image path at /src/js/object_data_product.js

--- src/js/object_data_product.js
+++ src/js/object_data_product.js
@@ -1,7 +1,7 @@
 export const dataProduct = {
   thinkpad_1: {
     product: "ThinkPad X1 Yoga Gen 7",
-    image: "../assets/tp-x1-y-g7.png",
+    image: "/assets/tp-x1-y-g7.png",
     detail: [
       "Powerful lightweight Intel® Evo™ 2-in-1 laptop",
       "FHD cameras(1920x1080), 4 mics & Dolby Voice®",
@@ -10,7 +10,7 @@ export const dataProduct = {
   },
   thinkpad_2: {
     product: "ThinkPad E15 Gen 4",
-    image: "../assets/tp-e15-g4.png",
+    image: "/assets/tp-e15-g4.png",
     detail: [
       "Durable 15.6″ Intel® Core™-powered business laptop",
       "Copious memory & storage, optional discrete graphics",
@@ -19,7 +19,7 @@ export const dataProduct = {
   },
   thinkpad_3: {
     product: "ThinkPad P15v Gen 3",
-    image: "../assets/tp-p15v-g3.png",
+    image: "/assets/tp-p15v-g3.png",
     detail: [
       "Powered by up to 12th Gen Intel® Core™ i7 processors",
       "ISV-certified, plus up to NVIDIA® RTX™ discrete graphics",
@@ -28,7 +28,7 @@ export const dataProduct = {
   },
   thinkpad_4: {
     product: "ThinkPad Z16 Gen 2 ",
-    image: "../assets/tp-z16-g2.png",
+    image: "/assets/tp-z16-g2.png",
     detail: [
       "Performance powerhouse with AMD Ryzen™ PRO 7040 H Series",
       "Composed of recycled aluminum & post-consumer plastic",
@@ -37,7 +37,7 @@ export const dataProduct = {
   },
   thinkpad_5: {
     product: "ThinkPad T14 Gen 4",
-    image: "../assets/tp-t14-g4.png",
+    image: "/assets/tp-t14-g4.png",
     detail: [
       "14″ business laptop w/ 13th Gen Intel® Core™ processor",
       "Highly configurable with emphasis on security & speed",
